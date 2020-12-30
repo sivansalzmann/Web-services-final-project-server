442bdb6d44683e8dd9671aca09729dac2e6c02a5
dekelme <61948586+dekelme@users.noreply.github.com>
2020-12-30 17:10:09 +0200

    Update asset.ctrl.js

--- controllers/asset.ctrl.js
+++ controllers/asset.ctrl.js
@@ -174,91 +174,35 @@ exports.assetDBController = {
 
 //         const apiAdd = axios.get('put huroko url...')
 //         .then(response) => {
+//             $(document).ready(function () {
+//                 google.maps.event.addDomListener(window, 'load', initialize);
+//             });
+            
+//             function initialize() {
+//                 var options = {
+//                     types: ['(regions)'],
+//                    }
+//                    var options2 = {
+//                     types: ['address'],
+//                    }
+//                    var options3 = {
+//                     types: ['counrty'],
+//                    }
+//                 var inputCity = document.getElementById('city');
+//                 var inputStreet = document.getElementById('street');
+//                 var inputCountry = document.getElementById('counrty');
+//                 var autocomplete = new google.maps.places.Autocomplete(inputCity,options);
+//                 var autocomplete2 = new google.maps.places.Autocomplete(inputStreet,options2);
+//                 var autocomplete3 = new google.maps.places.Autocomplete(inputCountry,options3);
+//                 // placeResult = autocomplete.getPlace();
+//                 // console.log(placeResult);//This will get only the address
+//                 // input.value = placeResult.name;
+//             }
 
 //         }
 //     }
 // }
 
-  // This sample uses the Autocomplete widget to help the user select a
-// place, then it retrieves the address components associated with that
-// place, and then it populates the form fields with those details.
-// This sample requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
+ 
 
-// let placeSearch;
-// let autocomplete;
-// const componentForm = {
-//   street_number: "short_name",
-//   route: "long_name",
-//   locality: "long_name",
-//   administrative_area_level_1: "short_name",
-//   country: "long_name",
-//   postal_code: "short_name",
-// };
-
-// function initAutocomplete() {
-//   // Create the autocomplete object, restricting the search predictions to
-//   // geographical location types.
-//   autocomplete = new google.maps.places.Autocomplete(
-//     document.getElementById("autocomplete"),
-//     { types: ["cities"] }
-//   );
-//   // Avoid paying for data that you don't need by restricting the set of
-//   // place fields that are returned to just the address components.
-//   autocomplete.setFields(["address_component"]);
-//   // When the user selects an address from the drop-down, populate the
-//   // address fields in the form.
-//   autocomplete.addListener("place_changed", fillInAddress);
-// }
-
-// function fillInAddress() {
-//   // Get the place details from the autocomplete object.
-//   const place = autocomplete.getPlace();
-
-//   for (const component in componentForm) {
-//     document.getElementById(component).value = "";
-//     document.getElementById(component).disabled = false;
-//   }
-
-//   // Get each component of the address from the place details,
-//   // and then fill-in the corresponding field on the form.
-//   for (const component of place.address_components) {
-//     const addressType = component.types[0];
-
-//     if (componentForm[addressType]) {
-//       const val = component[componentForm[addressType]];
-//       document.getElementById('city').value = val;
-//     }
-//   }
-// }
-
-//     $(document).ready(function () {
-//       google.maps.event.addDomListener(window, 'load', initialize);
-//    });
-   
-//    function initialize() {
-//        var input = document.getElementById('city');
-//        var autocomplete = new google.maps.places.Autocomplete(input);
-      
-// }
-// $(document).ready(function () {
-//     google.maps.event.addDomListener(window, 'load', initialize);
-// });
-
-// function initialize() {
-//     var options = {
-//         types: ['(regions)'],
-//        }
-//        var options2 = {
-//         types: ['address'],
-//         componentRestrictions: {country: 'il'}
-//        }
-//     var input = document.getElementById('city');
-//     var input2 = document.getElementById('street');
-//     var autocomplete = new google.maps.places.Autocomplete(input,options);
-//     var autocomplete2 = new google.maps.places.Autocomplete(input2,options2);
-//     placeResult = autocomplete.getPlace();
-//     console.log(placeResult);//This will get only the address
-//     input.value = placeResult.name;
-    
 
