9bf3dda3f8a2d6ed6dcd3d6e7ee7e66b85beaa13
dekelme <61948586+dekelme@users.noreply.github.com>
2021-01-27 19:08:43 +0200

    Create person.js

---
+++ Models/person.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+
+const personSchema = new Schema({
+    id: { type: Number},
+    FirstName: {type: String},
+    LastName: {type: String},
+    Gender: { type: String},
+    Phone: {type: String},
+    Email: { type: String},
+});
+
+const ownerSchema = new Schema ({
+    id: { type: Number},
+    Person: [personSchema],
+}, {collection: 'owners'});
+
+const renterSchema = new Schema({
+    id: { type: Number},
+    Person: [personSchema],
+    JobTitle: {type: String },
+    Budget: {type: String },
+}, { collection: 'renters'});
+
+
+const Owner = model('Owner', ownerSchema);
+const Renter = model('Renter', renterSchema);
+
+module.exports = {Owner, Renter};
