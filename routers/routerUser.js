30d99247036638551576ed8fb7f6fb2555fa3e50
sivansalzmann <sivansalzmann10@gmail.com>
2021-02-22 15:54:18 +0200

    Update routerUser.js

--- routers/routerUser.js
+++ routers/routerUser.js
@@ -4,7 +4,7 @@ const {userDBController} = require('../controllers/user.ctrl');
 const userRouter = new Router();  
 
 userRouter.get('/', userDBController.getUsers);
-userRouter.get('/:googleId', userDBController.getUser);
+userRouter.get('/:id', userDBController.getUser);
 userRouter.post('/', userDBController.addUser);
 userRouter.put('/:id', userDBController.updateUser);
 userRouter.delete('/:id', userDBController.deleteUser);
