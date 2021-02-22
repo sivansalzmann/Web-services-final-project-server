218a71190cfe744de6756802b8807e600417498f
sivansalzmann <sivansalzmann10@gmail.com>
2021-02-22 22:44:03 +0200

    clean code

--- routers/routergoogleAPI.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { googleAPIDBController } = require('../controllers/googleAPI.ctrl');
-
-const googleRouter = new Router();
-
-/* http://localhost:3000/api/assetAPI */
-googleRouter.get('/', googleAPIDBController.getAssetLocationFromAPI);
-
-module.exports = { googleRouter };
